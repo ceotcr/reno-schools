2ac2fc67589e6972acb7e26d1d6811677164b0d4
Chetan (TCR) Sapkal <68210482+ceotcr@users.noreply.github.com>
2025-08-31 14:33:43 +0530

    fix: update GET function to correctly handle params as a Promise

--- project/src/app/api/schools/[id]/route.ts
+++ project/src/app/api/schools/[id]/route.ts
@@ -4,12 +4,12 @@ import { parseForm, removeFile } from '@/lib/utils/fileUpload';
 
 export async function GET(
     _req: NextRequest,
-    { params }: { params: { id: string } }
+    { params }: { params: Promise<{ id: string }> }
 ) {
     try {
-        const id = parseInt(params.id);
+        const { id } = await params;
         const school = await prisma.school.findUnique({
-            where: { id },
+            where: { id: parseInt(id) },
         });
 
         if (!school) {
